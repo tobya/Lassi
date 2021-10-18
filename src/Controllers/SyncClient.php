decdf0045880571873181c7cb2d1599e011d609f
Toby Allen <toby@toflidium.com>
2021-10-18 18:22:53 +0100

    Use controller to sync

--- src/Controllers/SyncClient.php
+++ src/Controllers/SyncClient.php
@@ -5,29 +5,30 @@ namespace Lassi\Controllers;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Routing\Controller as BaseController;
 
-class SyncClient extends Controller
+class SyncClient extends BaseController
 {
-    public function updateusers($json)
+    public static function updateusers($json)
     {
         $data  = json_decode($json);
         collect($data->users)->each(function ($u){
-            $user = $this->FindorCreateUser($u->email);
+            $user = self::FindorCreateUser($u->email);
             $user->name = $u->name;
             $user->email = $u->email;
-            $user->password = $u->password;
+            $user->password = $u->lassipassword;
             $user->save();
             echo $user->id;
         });
     }
 
-    public function sync(){
+    public static function sync(){
 
                 $client = new Client();
-           $result = $client->request( 'GET',  config('lassi.server').  'api/sync/20210710');
+           $result = $client->request( 'GET',  config('lassi.server').  '/lassi/sync/20211010');
            $json = $result->getBody()->getContents();
+            self::updateusers($json);
 
-           $Sync = new SyncClient();
     }
 
     public static function FindOrCreateUser($email){
