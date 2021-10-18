01b59223991a79a63874579c60d28714dcaad5af
Toby Allen <toby@toflidium.com>
2021-10-18 18:24:30 +0100

    return lassipassword

--- src/Controllers/ApiSyncServer.php
+++ src/Controllers/ApiSyncServer.php
@@ -6,14 +6,21 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 
 class ApiSyncServer extends Controller
 {
     public function sync($lastsyncdate)
     {
         $users = User::where('updated_at','>',Carbon::parse($lastsyncdate))->get();
+       Log::debug(print_r($users,true));
 
-        return response()->json(['status'=>200,'users' => $users]);
+        $usersWithPassword = $users->map(function($user){
+          $user->lassipassword = $user->password;
+          return $user;
+        });
+
+        return response()->json(['status'=>200,'users' => $usersWithPassword]);
     }
 
     public function syncspecific(Request $request, $lassi_userid){
