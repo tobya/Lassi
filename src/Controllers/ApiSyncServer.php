f9952001a4755d69ac5dd1de07781b912d1e4a20
Toby Allen <toby@toflidium.com>
2021-10-18 21:06:59 +0100

    use new config values for token_ability

--- src/Controllers/ApiSyncServer.php
+++ src/Controllers/ApiSyncServer.php
@@ -3,15 +3,19 @@
 namespace Lassi\Controllers;
 
 use Carbon\Carbon;
+use http\Env\Response;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 class ApiSyncServer extends Controller
 {
     public function sync($lastsyncdate)
     {
+        if (!Auth::user()->tokenCan(config('lassi.token_ability'))){ return response('Not authorized',405);}
+
         $users = User::where('updated_at','>',Carbon::parse($lastsyncdate))->get();
        Log::debug(print_r($users,true));
 
