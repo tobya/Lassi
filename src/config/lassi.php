92ca0829b8284fc2d5e26bfa3bd084fcc43a903c
Toby Allen <toby@toflidium.com>
2021-12-18 10:54:31 +0000

    add whether to check permissions or not

--- src/config/lassi.php
+++ src/config/lassi.php
@@ -8,6 +8,7 @@ return [
         'url' => env('LASSI_SERVER'),
         'token_ability' => env('LASSI_TOKENABILITY', 'lassi_read'),
         'retriever' =>   Null,
+        'check_ability' => env('LASSI_CHECKABILITY',true),
         ],
     'client' => [
         'token' => env('LASSI_TOKEN'),
