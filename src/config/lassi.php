a75034db1ac630c2f5855d971c3ad21ee475b5c3
Toby Allen <toby@toflidium.com>
2021-12-18 10:51:38 +0000

    duplicate email action

--- src/config/lassi.php
+++ src/config/lassi.php
@@ -7,12 +7,15 @@ return [
     'server' => [
         'url' => env('LASSI_SERVER'),
         'token_ability' => env('LASSI_TOKENABILITY', 'lassi_read'),
-        'retriever' =>   null,
+        'retriever' =>   Null,
         ],
     'client' => [
         'token' => env('LASSI_TOKEN'),
         'server' => env('LASSI_SERVER'),
-    ]
-    
+        'duplicate_email_action' => 'overwrite' , // [overwrite, ignore, error]
+    ],
+
+
+
 
 ];
