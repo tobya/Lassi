54dcaa4f4deef9b9b01cb1d08680cfb14051f72f
Toby Allen <toby@toflidium.com>
2022-01-29 18:28:26 +0000

    tidy up

--- src/Controllers/SyncClient.php
+++ src/Controllers/SyncClient.php
@@ -2,6 +2,7 @@
 
 namespace Lassi\Controllers;
 
+use http\Client;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -35,68 +36,63 @@ class SyncClient extends BaseController
         echo "Attempting to update " . $data->users_count . " users";
         $GuardedFields = collect($this->guard);
         collect($data->users)->each(function ($u) use ($userFields, $GuardedFields) {
-            
+
             // First check if we should deal with this user at all.
             if (!$this->shouldHandle($u)){
                 return;
             }
-            
+
             $user = $this->FindorCreateUser($u->lassi_user_id);
             Log::Debug('Retrieved : ' . $user->name);
          //   dd(config('lassi.client.duplicate_email_action'));
             if (config('lassi.client.duplicate_email_action') == 'overwrite'){
-                if (!$user->exists) { // new user, check if emails match
-                   // dd($user);
-                   // echo 'does not exist';
+
+                // if user does not match lassi_user_id and we wish to overwrite then
+                // check if a user can be found matching email.
+                if (!$user->exists) {
                     $emaildup = User::where('email',$u->email)->first();
                     if ($emaildup){
+                        // set existing user
                         $user = $emaildup;
                     }
-                } else {
-                   // echo 'user exists';
-
-                  //  dd($user, 'yes');
                 }
             }
 
             // Loop through all fields on user table on client. Ignore specified fields and update
             // fields that exist in both client and incoming data.
-
             collect($userFields)->each(function ( $fieldname) use($user, $u, $GuardedFields){
-               // Log::Debug(json_encode($u));
+
+                // Only attempt to set fields that are not guarded.
                if (!$GuardedFields->contains( $fieldname)){
                    if ($fieldname == 'password'){
                         $user->password = $u->lassipassword;
                    } else {
-                        //   Log::Debug("user $fieldname =u $fieldname");
+                       // If field exists on client user model set it.
                        if (isset($u->{$fieldname})){
-                         //  Log::Debug("$fieldname set");
                         $user->{$fieldname} =$u->{$fieldname};
-                       //    Log::Debug("$fieldname set = " . $user->{$fieldname} );
                        }
                    }
                }
             });
 
-            $newUser = !$user->exists;
+            $isNewUser = !$user->exists;
             try {
                 $user->save();
             } catch ( \Exception $e) {
-                                $msg = "Error Happened: " . $e->getMessage() . '. Unable to create user - ' . json_encode($u);
+                                $msg = "[Lassi] Error Happened: " . $e->getMessage() . '. Unable to create user - ' . json_encode($u);
                                 echo $msg;
                                 Log::error($msg);
             }
-            
-            if ($newUser){
+
+            if ($isNewUser){
                 LassiUserCreated::dispatch($u, $user);
             } else {
                 LassiUserUpdated::dispatch($u, $user);
             }
 
-           // echo $user->id;
         });
 
-        $this->writeConfig($this->currentUpdate);
+       return  $this->writeConfig($this->currentUpdate);
     }
 
     public  function sync($data = null){
@@ -108,20 +104,23 @@ class SyncClient extends BaseController
         ])->asForm();
 
         try {
-        echo "Attempting to sync users from " . $this->lastUpdated() . "\n";
-        echo "Data " . json_encode($data) . "\n";
-            $result = $client->post(config('lassi.server.url')
-                                .  '/lassi/sync/'
-                                . urlencode( $this->lastUpdated())
+
+            $result = $client->post(config('lassi.server.url') .  '/lassi/sync/' . urlencode( $this->lastUpdated())
                                 ,['lassidata' => json_encode(  $data)]);
+
+            if ($result->status() <> 200){
+              Log::error('[Lassi:sync] Error Occurred: '. $result->status());
+              abort($result->status(),'Error Returned: ' . $result->status() . ' ' .  $result->getBody()->getContents());
+            }
+
         } catch ( \Exception $e) {
-            $msg = "Error Happened :" . $e->getMessage();
+            $msg =  $e->getMessage();
             Log::error($msg,['trace' => $e->getTrace()]);
             return $msg;
         }
 
            $json = $result->getBody()->getContents();
-           Log::debug($json);
+
           return  $this->updateusers($json);
 
     }
@@ -162,13 +161,13 @@ class SyncClient extends BaseController
             return '19000101';
         }
     }
-    
+
     public function shouldHandle($user){
         if (config('lassi.client.handler')){
             $classname = config('lassi.client.handler');
             $handler = new $classname();
             return $handler->Accept($user);
-        } 
+        }
         return true;
     }
 
