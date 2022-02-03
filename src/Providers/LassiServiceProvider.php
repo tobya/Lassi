8ad403246b2efae08fd68405ceca2b629b762a42
Toby Allen <toby@toflidium.com>
2022-02-03 06:43:36 +0000

    tidy up

--- src/Providers/LassiServiceProvider.php
+++ src/Providers/LassiServiceProvider.php
@@ -28,8 +28,7 @@ class LassiServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        Log::debug('boot');
+               
         $this->loadRoutesFrom(__DIR__ .'/../routes/lassi.php');
         $this->loadMigrationsFrom(__DIR__ . '/../migrations');
 
@@ -38,7 +37,6 @@ class LassiServiceProvider extends ServiceProvider
                 SyncLassi::class,
             ]);
         }
-
 
         $this->publishes([
             __DIR__.'/../config/lassi.php' => config_path('lassi.php'),
