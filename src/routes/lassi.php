179ed5e97951be165cdd2e8ebb7f32ffc9f9923f
Toby Allen <toby@cookingisfun.ie>
2022-04-26 06:19:33 +0100

    temp routes

--- src/routes/lassi.php
+++ src/routes/lassi.php
@@ -5,7 +5,13 @@ use Lassi\Controllers\ApiSyncServer;
 
 
 
+Route::middleware(['auth:sanctum'])->post('lassi/sync/all',[ApiSyncServer::class,'syncall']);
+Route::middleware(['auth:sanctum'])->post('lassi/sync/user/{lassiuserid}',[ApiSyncServer::class,'syncuser']);
+Route::get('lassi/sync/user/{lassiuserid}',[ApiSyncServer::class,'syncuser']);
+//Route::get('lassi/sync/all',[ApiSyncServer::class,'syncall']);
 Route::middleware(['auth:sanctum'])->post('lassi/sync/{lastsyncdate}/{marker?}',[ApiSyncServer::class,'sync']);
+Route::get('lassi/sync/{lastsyncdate}/{marker?}',[ApiSyncServer::class,'sync']);
+
 
 // Update details of a specific user - new password etc.
 Route::middleware(['auth:sanctum'])->post('lassi/update/{lassiuserid}',[ApiSyncServer::class,'updateuser']);
