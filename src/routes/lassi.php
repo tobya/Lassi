9f4480da7049ebc272072ec0a2a34f08f30551ee
Toby Allen <toby@cookingisfun.ie>
2022-10-22 15:12:41 +0100

    update routes

--- src/routes/lassi.php
+++ src/routes/lassi.php
@@ -7,8 +7,12 @@ use Lassi\Controllers\ApiSyncServer;
 
 Route::middleware(['auth:sanctum'])->post('lassi/get/all',[ApiSyncServer::class,'getall']);
 Route::middleware(['auth:sanctum'])->post('lassi/sync/user/{lassiuserid}',[ApiSyncServer::class,'syncuser']);
-Route::get('lassi/sync/user/{lassiuserid}',[ApiSyncServer::class,'syncuser']);
-//Route::get('lassi/sync/all',[ApiSyncServer::class,'syncall']);
+Route::middleware(['auth:sanctum'])->get ('lassi/sync/user/{lassiuserid}',[ApiSyncServer::class,'syncuser']);
+Route::middleware(['auth:sanctum'])->get ('lassi/sync/all',[ApiSyncServer::class,'syncall']);
+Route::middleware(['auth:sanctum'])->get ('lassi/sync/ids/{lastsyncdate}',[ApiSyncServer::class,'getsyncids']);
+Route::middleware(['auth:sanctum'])->post ('lassi/sync/ids/{lastsyncdate}',[ApiSyncServer::class,'getsyncids']);
+Route::middleware(['auth:sanctum'])->get ('lassi/sync/count/{lastsyncdate}',[ApiSyncServer::class,'count']);
+Route::middleware(['auth:sanctum'])->post ('lassi/sync/count/{lastsyncdate}',[ApiSyncServer::class,'count']);
 Route::middleware(['auth:sanctum'])->post('lassi/sync/{lastsyncdate}/{marker?}',[ApiSyncServer::class,'sync']);
 Route::get('lassi/sync/{lastsyncdate}/{marker?}',[ApiSyncServer::class,'sync']);
 
