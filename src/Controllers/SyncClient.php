3fa65707646c2b93a40a9413878658fbd8bbf592
Toby Allen <toby@cookingisfun.ie>
2023-04-01 22:04:07 +0100

    update writeconfig to use cache

--- src/Controllers/SyncClient.php
+++ src/Controllers/SyncClient.php
@@ -3,6 +3,7 @@
 namespace Lassi\Controllers;
 
 use http\Client;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -28,7 +29,7 @@ class SyncClient extends BaseController
         'two_factor_recovery_codes',
         'remember_token',
         ];
-    public $queue = 'default';
+    public $queue = null;
 
     public  function updateusers($json)
     {
@@ -36,13 +37,13 @@ class SyncClient extends BaseController
         Log::debug($json);
 
       //  dd($userFields);
-        echo "Attempting to update " . $data->users_count . " users. Pushed onto Queue.";
+        echo "Attempting to update " . $data->users_count . " users";
 
         collect($data->users)->each(function ($u)  {
             UpdateUserJob::dispatch($u);
         });
 
-       return  $this->writeConfig($data->lastitem_updated_at);
+       return  $this->writeConfig($this->currentUpdate);
     }
 
     public function syncAll($data = null){
@@ -75,24 +76,23 @@ class SyncClient extends BaseController
             return $msg;
         }
 
-        $json = $result->getBody()->getContents();
+           $json = $result->getBody()->getContents();
 
-        $UserList = json_decode($json);
-        collect($UserList->userids)->each(function ($lassi_user_id){
-            echo "\n Adding " , $lassi_user_id;
-           SyncUserJob::dispatch($lassi_user_id)->onQueue($this->queue);
-        });
-
+            $UserList = json_decode($json);
+            collect($UserList->userids)->each(function ($lassi_user_id){
+               SyncUserJob::dispatch($lassi_user_id)->onQueue($this->queue);
+            });
 
 
-        $this->writeConfig($this->currentUpdate);
+
+            $this->writeConfig($this->currentUpdate);
         return 'Added ' . $UserList->userids_count . ' ids to Job list';
     }
 
 
 
     public  function sync($data = null){
-       $this->currentUpdate = now();
+       $this->currentUpdate = now('utc');
         $client = Http::withHeaders(
         [
             'Accept'        => 'application/json',
@@ -100,30 +100,14 @@ class SyncClient extends BaseController
         ])->asForm();
 
         try {
-
-            $count_result = $client->post(config('lassi.server.url') .  '/lassi/sync/count/' . urlencode( $this->lastUpdated())
+            $url = config('lassi.server.url') .  '/lassi/sync/' . urlencode( $this->lastUpdated());
+            $result = $client->post($url
                                 ,['lassidata' => json_encode(  $data)]);
-             $json = $count_result->getBody()->getContents();
-             $count = json_decode($json);
-             Log::debug('about to log json');
-             Log::debug($json);
-             //Log::debug( print_r($count,true));
-            if ($count->users_count > 100){
-                return $this->syncSingle();
 
-            } else {
-
-
-                $result = $client->post(config('lassi.server.url') .  '/lassi/sync/' . urlencode( $this->lastUpdated())
-                                    ,['lassidata' => json_encode(  $data)]);
-
-                if ($result->status() <> 200){
-                  Log::error('[Lassi:sync] Error Occurred: '. $result->status());
-                  abort($result->status(),'Error Returned: ' . $result->status() . ' ' .  $result->getBody()->getContents());
-                }
-                $json = $result->getBody()->getContents();
-                return  $this->updateusers($json);
-
+            if ($result->status() <> 200){
+              Log::error('[Lassi:sync] Error Occurred: '. $result->status());
+              Log::info('[Lassi:sync] ' . $url  );
+              abort($result->status(),'Error Returned: ' . $result->status() . ' ' .  $result->getBody()->getContents());
             }
 
         } catch ( \Exception $e) {
@@ -132,70 +116,27 @@ class SyncClient extends BaseController
             return $msg;
         }
 
+           $json = $result->getBody()->getContents();
 
-
-    }
-
-    public function syncSingle(){
-        //lassi/sync/ids/{lastsyncdate}
-        $client = Http::withHeaders(
-        [
-            'Accept'        => 'application/json',
-            'Authorization' => 'Bearer ' . config('lassi.client.token') ,
-        ])->asForm();
-        try {
-            $result = $client->post(config('lassi.server.url') .  '/lassi/sync/ids/' . urlencode( $this->lastUpdated()));
-
-            $json = $result->getBody()->getContents();
-            Log::debug($json);
-            $UserList = json_decode($json);
-
-            collect($UserList->user_ids)->each(function ($lassi_user_id){
-               SyncUserJob::dispatch($lassi_user_id)->onQueue($this->queue);
-            });
-            Log::debug('[Past collect');
-        } catch ( \Exception $e) {
-            $msg =  $e->getMessage();
-            Log::error($msg,['trace' => $e->getTrace()]);
-            return $msg;
-        }
-        $this->writeConfig($UserList->lastitem_updated_at);
-
-        return 'Added v ' . $UserList->user_ids_count . ' ids to Job list';
+          return  $this->updateusers($json);
 
     }
 
 
 
     public  function writeConfig($lastupdate){
-        $configfile = storage_path('app/lassi/lassi.config');
-        if (file_exists($configfile)){
-            $file = File::get($configfile);
-            $config = json_decode($file,true);
-        } else {
-            File::ensureDirectoryExists(pathinfo($configfile,PATHINFO_DIRNAME));
-            $config = [];
+        if (is_object($lastupdate)){
+            // must be a string, and cannot contain spaces - garbled by urlencoding.
+            $lastupdate = $lastupdate->format('Ymd\TH:i:s');
         }
-        $config['lastupdate'] = $lastupdate;
-        File::put($configfile, json_encode($config));
-        return $configfile;
+        Cache::put('lassi.config', ['lastupdate' => $lastupdate]);
+
     }
 
-    /**
-     *
-     * @return mixed|string
-     */
     public function lastUpdated()
     {
-        $configfn = storage_path('app/lassi/lassi.config');
-        if (file_exists($configfn)){
-            $file = File::get($configfn);
-            $config = json_decode($file,true);
-          //  dd($config);
-            return $config['lastupdate'];
-        } else {
-            return '19000101';
-        }
+      $value =  Cache::get('lassi.config',['lastupdate' => '20000101']);
+        return $value['lastupdate'];
     }
 
 
