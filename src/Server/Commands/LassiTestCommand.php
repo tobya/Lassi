0e767eb8ad219f0d475ad7ccba466db3e65212bd
Toby Allen <toby@cookingisfun.ie>
2023-08-06 10:50:18 +0100

    server command test

---
+++ src/Server/Commands/LassiTestCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+  namespace Lassi\Commands;
+
+  use Illuminate\Console\Command;
+  use App\Models\User;
+
+  class LassiTestCommand extends Command
+  {
+    protected $signature = 'lassi:test {--count=} {--modify} {--create}';
+
+    protected $description = 'Test command to modify and create users';
+
+    public function handle(): void
+    {
+        $count = $this->option('count',0);
+        if ($count > 0){
+            if ($this->option('modify')){
+              $users =  User::where('id','>',rand(4,18000))->take($count)->get();
+              $users->each(function ($u){
+                  $u->touch();
+                  $u->save();
+              });
+            }
+
+            if($this->option('create')){
+                User::Factory()->Count($count)->create();
+            }
+
+        }
+
+    }
+  }
